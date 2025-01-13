b61f4f6af6dbca847598a823576ffc7a0b80fad3
sh4dow <shadoxxhd@users.noreply.github.com>
2025-01-13 09:30:16 +0100

    Update settings.lua
    added "mqu-speed-magnitude" setting

--- settings.lua
+++ settings.lua
@@ -28,5 +28,12 @@ data:extend({
 		name = "mqu-artillery-wagon-changes",
 		setting_type = "startup",
 		default_value = true
+	},
+	{
+		type = "string-setting",
+		name = "mqu-speed-magnitude",
+		setting_type = "startup",
+		default_value = "0.3",
+		allowed_values = {"0.05", "0.15", "0.3", "0.5", "1.0"}
 	}
 })
